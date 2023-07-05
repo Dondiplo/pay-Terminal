4bdced4e20d97cd6f8ba5290b30e4c75a1489345
Dipo Desmond-eke <dipodesmond-eke@Dipos-MacBook-Pro.local>
2023-07-05 15:15:29 +0100

    fourth commit

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,43 +4,95 @@ import android from "/public/assets/image 2.svg";
 import Image from "next/image";
 import Link from "next/link";
 import phone1 from "/public/assets/Group 508.svg";
-import rectangle7 from "public/assets/Rectangle 7.svg"
-import logos from "/public/assets/Group 509.svg"
+import rectangle7 from "public/assets/Rectangle 7.svg";
+import logos from "/public/assets/Group 509.svg";
+import vector from "/public/assets/Vector.svg";
+import group462 from "/public/assets/Group 462.svg";
+import group463 from "/public/assets/Group 463.svg";
+import group464 from "/public/assets/Group 464.svg";
+import line from "/public/assets/Line 1.svg";
+import "../app/app.css";
 
 export default function Hero() {
   return (
     <div className="lg:container lg:mx-auto">
-      <div className="bg-cover bg-center h-[950px] relative" style={{ backgroundImage:"url(/assets/Rectangle%202.svg)" }}>
-      
-      <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 md:grid-rows-1 gap-4 pt-28">
-  <div className="order-first md:order-first justify-center mt-0 md:mt-20 ml-16">
-    <h1 className="text-[#3C5F00] font-thin lg:text-[50px] md:text-[35px]">Fastest way to make</h1>
-    <h2 className="text-[#3C5F00] font-extrabold lg:text-[50px] md:text-[35px]" >Secured payment.</h2>
-    <p className="lg:text-[18px] md:text-[15px] pt-12 lg:leading-[33px]">Send money to friends, support local businesses, pay online <br />
-     vendors, and manage subscriptions—all in a few clicks. With <br />
-      our payment app, streamline your transactions effortlessly.</p>
-      
+      <div
+        className="bg-cover bg-center h-[950px] relative"
+        style={{ backgroundImage: "url(/assets/Rectangle%202.svg)" }}
+      >
+        <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 md:grid-rows-1 gap-4 pt-28">
+          <div className="order-first md:order-first justify-center mt-0 md:mt-20 ml-16">
+            <h1 className="text-[#3C5F00] font-thin lg:text-[50px] md:text-[45px]">
+              Fastest way to make
+            </h1>
+            <h2 className="text-[#3C5F00] font-extrabold lg:text-[50px] md:text-[45px]">
+              Secured payment.
+            </h2>
+            <p className="lg:text-[18px] md:text-[15px] font-[400] pt-12 lg:leading-[33px]">
+              Send money to friends, support local businesses, pay online <br />
+              vendors, and manage subscriptions—all in a few clicks. With <br />
+              our payment app, streamline your transactions effortlessly.
+            </p>
 
-      <div className=" md:flex space-x-4 pt-12">
-          <Link href="https://play.google.com/store/apps/details?id=com.payterminal.payterminal">
-            <Image src={apple} alt="logo" />
-          </Link>
-          <Link href="/">
-            <Image src={android} alt="logo" />
-          </Link>
+            <div className=" md:flex space-x-4 pt-12">
+              <Link href="https://play.google.com/store/apps/details?id=com.payterminal.payterminal">
+                <Image src={apple} alt="logo" />
+              </Link>
+              <Link href="/">
+                <Image src={android} alt="logo" />
+              </Link>
+            </div>
+          </div>
+          <div className="order-last md:order-last justify-center">
+            <Image src={phone1} alt="phone1" />
+          </div>
         </div>
-  </div>
-  <div className="order-last md:order-last justify-center">
-   <Image src={phone1} alt="phone1"/>
-  </div>
-</div>
-
-    
       </div>
-      <div className="rounded-3xl bg-zinc-50  mx-auto py-8 mt-[35rem] md:mt-0 ">
-<Image className="mx-auto" src={logos} alt="logos"/>
-</div>
+      <div className="rounded-3xl bg-zinc-50  mx-auto py-8 mt-[35rem] md:mt-0  w-[90%]">
+        <Image className="mx-auto" src={logos} alt="logos" />
+      </div>
 
+      <div className="flex justify-end pt-8">
+        <Image className="" src={vector} alt="vector" />
+      </div>
+
+      {/* section2 */}
+      <section className="justify-content" id="hero2">
+        <div>
+          <h1 className="font-bold text-[43px] text-[#2d3247]  text-center">
+            How it Works
+          </h1>
+        </div>
+
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 justify-center mx-auto pt-12 items-center ">
+          <div className=" h-80 mx-auto ">
+            <Image src={group462} alt="group462" />
+            <h1 className="font-bold text-center pt-4">Download Mobile App</h1>
+            <p className="text-center pt-4">
+              To get started, simply download the <br />
+              Payterminal mobile application from the <br />
+              app store or play store
+            </p>
+          </div>
+          <div className="h-80 mx-auto">
+            <Image src={group463} alt="group463" />
+            <h1 className="font-bold text-center ">Create an account</h1>
+            <p className="text-center pt-4">
+            Sign up on the mobile app in less than <br />
+             2 mins by providing the required KYC <br />
+              details.
+            </p>
+          </div>
+          <div className="h-80 mx-auto">
+            <Image src={group463} alt="group463" />
+            <h1 className="font-bold text-center ">Add a debit card</h1>
+            <p className="text-center pt-4">
+            Add a debit card or fund your account <br />
+             and you’re good to go
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,4 +1,5 @@
-import React,{useState} from "react";
+"use client"
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { GrClose } from "react-icons/gr";
 import Link from "next/link";
@@ -6,34 +7,42 @@ import Image from "next/image";
 import logo from "/public/assets/logo 1.svg";
 import apple from "/public/assets/image 1.svg";
 import android from "/public/assets/image 2.svg";
+
 export default function Nav() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const handleCloseMenu = () => {
+    setMobileMenuOpen(false);
+  };
+
   return (
-    <div className="mx-10   pt-12  ">
+    <div className="mx-10 pt-12">
       <div className="flex justify-between items-center relative">
-        <Link href="/">
-          {" "}
-          <Image src={logo} alt="logo" />{" "}
+        <Link href="/"
+        onClick={handleCloseMenu}>
+          <Image src={logo} alt="logo" />
+          
         </Link>
 
-        <div className=" space-x-4 lg:space-x-10 md:flex hidden">
-          <Link className="hover:font-bold " href="/us/home">
-            {" "}
+        <div className="space-x-4 lg:space-x-10 md:flex hidden">
+          <Link className="hover:font-bold" href="/us/home">
             Home
           </Link>
-          <Link className="hover:font-bold " href="/us/about-us">
-            {" "}
+          <Link className="hover:font-bold" href="/us/about-us">
             About Us
           </Link>
-          <Link className="hover:font-bold " href="/us/faq">
-            {" "}
+          <Link className="hover:font-bold" href="/us/faq">
             FAQ
           </Link>
-          <Link className="hover:font-bold " href="/us/contact-us">
-            {" "}
-            Contact Us{" "}
+          <Link className="hover:font-bold" href="/us/contact-us">
+            Contact Us
           </Link>
         </div>
-        <div className=" md:flex space-x-4 hidden">
+        <div className="md:flex space-x-4 hidden">
           <Link href="https://play.google.com/store/apps/details?id=com.payterminal.payterminal">
             <Image src={apple} alt="logo" />
           </Link>
@@ -42,29 +51,52 @@ export default function Nav() {
           </Link>
         </div>
 
-        {/* mobile*/}
+        {/* mobile */}
         <div className="md:hidden">
-          <GiHamburgerMenu />
-          <GrClose />
+          <GiHamburgerMenu
+            onClick={handleToggleMenu}
+            className={isMobileMenuOpen ? "hidden" : "cursor-pointer"}
+          />
+          <GrClose
+            onClick={handleCloseMenu}
+            className={isMobileMenuOpen ? "cursor-pointer" : "hidden"}
+          />
         </div>
       </div>
-      {/* mobile menu */}
-      <div className="md:hidden  absolute fixed bg-white z-10 self-end py-8 space-y-6 bg-white sm:w-auto left-6 right-6 drop-shadow-md flex flex-col justify-center items-center">
-  <Link className="hover:font-bold" href="/us/home">
-    Home
-  </Link>
-  <Link className="hover:font-bold" href="/us/about-us">
-    About Us
-  </Link>
-  <Link className="hover:font-bold" href="/us/faq">
-    FAQ
-  </Link>
-  <Link className="hover:font-bold" href="/us/contact-us">
-    Contact Us
-  </Link>
-</div>
 
-      
+      {/* mobile menu */}
+      {isMobileMenuOpen && (
+        <div className="md:hidden absolute fixed bg-white z-10 self-end py-8 space-y-6 bg-white sm:w-auto left-6 right-6 drop-shadow-md flex flex-col justify-center items-center">
+          <Link
+            className="hover:font-bold"
+            href="/us/home"
+            onClick={handleCloseMenu}
+          >
+            Home
+          </Link>
+          <Link
+            className="hover:font-bold"
+            href="/us/about-us"
+            onClick={handleCloseMenu}
+          >
+            About Us
+          </Link>
+          <Link
+            className="hover:font-bold"
+            href="/us/faq"
+            onClick={handleCloseMenu}
+          >
+            FAQ
+          </Link>
+          <Link
+            className="hover:font-bold"
+            href="/us/contact-us"
+            onClick={handleCloseMenu}
+          >
+            Contact Us
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
